c75a135984066cfd7fa16aa75468a2ba5c9be890
Mark Xu <isafesoft@hotmail.com>
2018-12-14 13:36:16 -0500

    request param binding

--- src/main/java/com/test/mk/AgentController.java
+++ src/main/java/com/test/mk/AgentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,8 +22,10 @@ public class AgentController {
     }
 
     @RequestMapping("/processForm2")
-    public String modelTest(HttpServletRequest request, Model model) {
-        String name = request.getParameter("agentName");
+    //public String modelTest(HttpServletRequest request, Model model) {
+    public String modelTest(
+            @RequestParam("agentName") String name, // param binding
+            Model model) {
         name = name.toUpperCase();
         String res = "Responded, " + name;
 
