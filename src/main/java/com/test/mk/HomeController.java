6e2b1dc1624fe714ba02409788ec4f5de2467325
Mark Xu <isafesoft@hotmail.com>
2018-12-14 10:46:22 -0500

    first mvc, url/alex

---
+++ src/main/java/com/test/mk/HomeController.java
@@ -0,0 +1,14 @@
+package com.test.mk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = {"/alex"}, method = RequestMethod.GET)
+    public String showPage() {
+        System.out.println("hibaby");
+        return "main-menu";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mk
+  Date: 2018-12-13
+  Time: 14:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+</head>
+<body>
+
+<h2>Spring MVC</h2>
+</body>
+</html>
